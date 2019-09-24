2e5deced937f63ed4896e6b6ace3c146017a8c8c
jamesdraney <54819912+jamesdraney@users.noreply.github.com>
2019-09-23 21:39:44 -0400

    Add files via upload

---
+++ james draney tree.py
@@ -0,0 +1,22 @@
+height = int(input( "Enter tree height: "))
+
+
+
+for row in range (height):
+    for count in range (height-row):
+        print(end = " ")
+    for count in range (2 * row + 1):
+        print (end = "#")
+    print(" ")
+    print ()
+print ((height) * " ", end = "#")
+
+
+
+
+    
+
+
+        
+
+        
